88d10044d39c2a593f1003ce361ab7ca445a48fb
Aunney <101056519+Aunney@users.noreply.github.com>
2023-04-29 18:06:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function getInfo() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "Supakarn" && password == "Pungboonpaisan") {
+        window.location.assign("hbd.html");
+    }
+
+}
+   
